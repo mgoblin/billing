d43bde799b45e6468784b2cae01661a942c4a6c9
Michael Golovanov <mike.golovanov@gmail.com>
2017-07-21 12:56:34 -0700

    Add spring boot

---
+++ src/main/scala/ru/sbt/openapi/billing/Application.scala
@@ -0,0 +1,7 @@
+package ru.sbt.openapi.billing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+}
--- src/main/scala/ru/sbt/openapi/billing/Main.scala
+++ src/main/scala/ru/sbt/openapi/billing/Main.scala
@@ -1,12 +1,14 @@
 package ru.sbt.openapi.billing
 
 import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.boot.SpringApplication
 
 object Main {
 
   val logger: Logger = LoggerFactory.getLogger(Main.getClass)
 
   def main(args: Array[String]): Unit = {
-    logger.debug("Hello, billing")
+    logger.debug("Start billing application")
+    SpringApplication.run(Array[AnyRef](classOf[Application]), args)
   }
 }
