811afcaf767bd416a8cc1d5464dd0512c71f95df
Michael Golovanov <mike.golovanov@gmail.com>
2017-07-22 02:21:01 -0700

    Create db using ansible

---
+++ sql/drop-create-db.sql
@@ -0,0 +1,29 @@
+
+DROP DATABASE IF EXISTS tariffs;
+DROP USER IF EXISTS openapi;
+
+-- User: openapi
+CREATE USER openapi WITH
+  LOGIN
+  SUPERUSER
+  INHERIT
+  CREATEDB
+  CREATEROLE
+  NOREPLICATION
+  PASSWORD 'qazwsx';
+
+-- Database: tariffs
+CREATE DATABASE tariffs
+    WITH
+    OWNER = openapi
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+\connect tariffs;
+
+-- SCHEMA: tariffs_schema
+CREATE SCHEMA tariffs_schema
+    AUTHORIZATION openapi;
